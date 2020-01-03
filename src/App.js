d03c00cfac43d6350c9749200f1e4a24f0087527
Ethan Tal Jaffe <jaffe413@gmail.com>
2020-01-03 16:57:48 +0200

    added react-router-dom to project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
 import CompareShowcase from "./pages/compare-showcase/compare-showcase";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import TextToPathShowcase from "./pages/text-to-path-showcase/text-to-path-showcase";
 
 function App() {
     return (
-        <div className="App">
-            <div className="showcases-container">
-                <TextToPathShowcase/>
+        <Router>
+            <div className="App">
+                <div className="showcases-container">
+                    <Switch>
+                        <Route path="/text-on-path-showcase">
+                            <TextToPathShowcase/>
+                        </Route>
+                        <Route path="/compare-showcase">
+                            <CompareShowcase/>
+                        </Route>
+                        <Route path="/">
+                            <TextToPathShowcase/>
+                        </Route>
+                    </Switch>
+                </div>
             </div>
-        </div>
+        </Router>
     );
 }
 
